c8b74a639cc4492cb1031afa5ab38417ee2cb660
admin <admin@afip-ddba142514.afip.gov.ar>
2015-07-02 11:23:11 -0300

    Borré los "public" de los métodos del ABBTDACatalogo

--- src/tdas/ABBTDACatalogo.java
+++ src/tdas/ABBTDACatalogo.java
@@ -16,45 +16,45 @@ public interface ABBTDACatalogo {
 	/* Inicializa la estructura.
 	 * Pre-condiciones: No posee.
 	 * Pos-condiciones: La estructura queda inicializada. */
-	public void inicializar();
+	void inicializar();
 	
 	/* Indica si la estructura tiene elementos o no (true: no tiene | false: tiene).
 	 * Pre-condiciones: La estructura tiene que estar inicializada. 
 	 * Pos-condiciones: La estructura no se ve modificada. */
-	public boolean arbolVacio();
+	boolean arbolVacio();
 	
 	/* Agrega un libro a la estructura. Si el género del libro existe, se incorpora a ese género. 
 	 * Si el género no existe, se crea primero el género y luego se incorpora el libro.
 	 * Pre-condiciones: La estructura tiene que estar inicializada.
 	 * Pos-condiciones: La estructura se ve modificada con un elemento en más. */
-	public void agregarLibro(Libro libro);
+	void agregarLibro(Libro libro);
 	
 	/* Elimina un género de la estructura y todos sus libros. 
 	 * Reacomoda los restantes géneros del catálogo.
 	 * Pre-condiciones: La estructura tiene que estar inicializada.
 	 * Pos-condiciones: La estructura se ve modificada con un elemento en menos, si existe el género. */
-	public void eliminarGenero(Genero genero);
+	void eliminarGenero(Genero genero);
 	
 	/* Elimina uno o más libros de la estructura. Si el género del libro no existe o el libro no existe en su género, no hace nada.
 	 * Si es el único libro, elimina también el género.
 	 * Pre-condiciones: La estructura tiene que estar inicializada.
 	 * Pos-condiciones: La estructura se ve modificada, con tantas ocurrencias del libro existan, en menos, 
 	 * si tanto el género del libro como el libro existen. */
-	public void eliminarLibro(Libro libro);
+	void eliminarLibro(Libro libro);
 	
 	/* Devuelve el Género que está en la raíz del catálogo o nulo si el árbol es nulo.
 	 * Pre-condiciones: La estructura tiene que estar inicializada.
 	 * Pos-condiciones: No posee. */
-	public Genero obtenerGenero();
+	Genero obtenerGenero();
 	
 	/* Devuelve el catálogo de libros asociado al género cuyo nombre es alfabéticamente anterior al género raíz, o nulo si no existe.
 	 * Pre-condiciones: La estructura tiene que estar inicializada.
 	 * Pos-condiciones: No posee. */
-	public ABBTDACatalogo hijoIzq();
+	ABBTDACatalogo hijoIzq();
 	
 	/* Devuelve el catálogo de libros asociado al género cuyo nombre es alfabéticamente anterior al género raíz, o nulo si no existe.
 	 * Pre-condiciones: La estructura tiene que estar inicializada.
 	 * Pos-condiciones: No posee. */
-	public ABBTDACatalogo hijoDer();
+	ABBTDACatalogo hijoDer();
 		
 }
